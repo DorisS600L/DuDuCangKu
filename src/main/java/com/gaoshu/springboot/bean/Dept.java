c59bdf418d786a04a17f580d9fb928e83c2fe775
DuDuCangKu <2211877467@qq.com>
2020-04-10 11:19:41 +0800

    新增部门实体类

---
+++ src/main/java/com/gaoshu/springboot/bean/Dept.java
@@ -0,0 +1,22 @@
+package com.gaoshu.springboot.bean;
+
+public class Dept {
+    private  int deptno;
+    private String dname;
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+}
